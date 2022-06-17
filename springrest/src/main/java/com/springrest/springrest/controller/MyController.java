4066c94f29098a09f21f1602cf0056d9131ce27c
Shilpa <parasianshilpa@gmail.com>
2022-06-17 14:01:37 -0400

    getting a single course

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.springrest.entities.Course;
@@ -30,8 +31,11 @@ public class MyController {
 		return this.courseService.getCourses();
 	}
 	
-	
-	
+	//gettting a single course
+	@GetMapping("/course/{courseId}")
+	public Course getCourse(@PathVariable String courseId) {
+		return this.courseService.getCourse(Long.parseLong(courseId));
+	}
 	
 
 }
