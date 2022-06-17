2bf53ac9b3b6f9ab93de4a1d9f0106d04b73c820
Shilpa <parasianshilpa@gmail.com>
2022-06-17 14:32:17 -0400

    adding post method to add courses

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.springrest.entities.Course;
@@ -32,10 +34,18 @@ public class MyController {
 	}
 	
 	//gettting a single course
-	@GetMapping("/course/{courseId}")
+	@GetMapping("/courses/{courseId}")
 	public Course getCourse(@PathVariable String courseId) {
 		return this.courseService.getCourse(Long.parseLong(courseId));
 	}
 	
+	//adding a single course
+	@PostMapping("/courses")
+	public Course addCourse(@RequestBody Course course) {
+		 return this.courseService.addCourse(course);
+	}
+	
 
 }
+
+
